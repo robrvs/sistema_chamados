f10212795676c90272800980873e1d7f93c619b5
robrvs <39736736+robrvs@users.noreply.github.com>
2026-06-13 18:06:08 -0400

    Fix: Atualizado armazenamento estático para padrão do Django 6

--- sistema_chamados/settings.py
+++ sistema_chamados/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,11 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
